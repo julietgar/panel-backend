51e6a8713c0aeb77ca4d54cca699724a00bb4353
Juliet Garcia <julietgar.duque@gmail.com>
2022-03-24 17:30:38 -0300

    prevent to insert data in the `update_at` column automatically

--- app/Models/Metric.php
+++ app/Models/Metric.php
@@ -7,10 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Metric extends Model
 {
     /**
+     * Prevent to insert in the `update_at` column automatically
+     */
+    public $timestamps = false;
+    
+    /**
      * The attributes that are mass assignable.
      *
      */
     protected $fillable = [
+        'machine_id',
         'device_id',
         'data',
         'date_from',
